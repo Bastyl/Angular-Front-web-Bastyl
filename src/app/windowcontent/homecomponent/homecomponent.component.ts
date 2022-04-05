3f8e359e33ed1e811378a38e7ad0e2d2b74b2557
basty <basty@bastys-MacBook-Pro.local>
2022-04-05 01:32:13 -0400

    changes

--- src/app/windowcontent/homecomponent/homecomponent.component.ts
+++ src/app/windowcontent/homecomponent/homecomponent.component.ts
@@ -7,9 +7,44 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomecomponentComponent implements OnInit {
 
+  images = ["./../../../assets/images/angular.png","./../../../assets/images/netcore.png","./../../../assets/images/mongodblogo.png"];
+  actualImage: string;
+  changeBackgroundCounter = 0;
+
   constructor() { }
 
   ngOnInit(): void {
+    this.actualImage = this.images[0];
+    setInterval(() => {
+      this.changeBackgroundCounter++;
+      console.log(this.changeBackgroundCounter);
+      if (this.changeBackgroundCounter > this.images.length - 1) {
+        this.changeBackgroundCounter = 0;
+      }
+      this.actualImage = this.images[this.changeBackgroundCounter];
+      console.log(this.actualImage)
+    }, 5000);
+  }
+
+  scroll(id: any) {
+    console.log(`scrolling to ${id}`);
+    let el = document.getElementById(id);
+    el.scrollIntoView({behavior: 'smooth'});
+  }
+
+  getImage(){
+    this.actualImage = this.images[0];
+    setInterval(() => {
+      this.changeBackgroundCounter++;
+      console.log(this.changeBackgroundCounter);
+      if (this.changeBackgroundCounter > this.images.length - 1) {
+        this.changeBackgroundCounter = 0;
+      }
+      this.actualImage = this.images[this.changeBackgroundCounter];
+      console.log(this.actualImage)
+    }, 5000);
+    console.log(this.actualImage)
+    return this.actualImage;
   }
 
 }
